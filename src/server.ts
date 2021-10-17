50a2855be3c775caa172f29f7f909dec42fb79d2
Carlos Indriago <carlos.indriago@tsoftglobal.com>
2021-10-17 15:19:26 -0300

    Enabled cors

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,21 @@
-import express from "express";
-import { connectToDatabase } from "./services/database.service"
-import { cardsRouter } from "./routes/cards.router";
+import express from 'express';
+import { connectToDatabase } from './services/database.service';
+import { cardsRouter } from './routes/cards.router';
+import cors from 'cors';
 
 const app = express();
-const port = 8080; // default port to listen
+const port = 81; // default port to listen
 
 connectToDatabase()
-    .then(() => {
-        app.use("/cards", cardsRouter);
+  .then(() => {
+    app.use(cors())
+    app.use('/cards', cardsRouter);
 
-        app.listen(process.env.PORT || 81, () => {
-            console.log(`Server started at http://localhost:${port}`);
-        });
-    })
-    .catch((error: Error) => {
-        console.error("Database connection failed", error);
-        process.exit();
+    app.listen(process.env.PORT || port, () => {
+      console.log(`Server started at http://localhost:${port}`);
     });
+  })
+  .catch((error: Error) => {
+    console.error('Database connection failed', error);
+    process.exit();
+  });

446e33c93a928235b2099434b910ac76b190b660
Carlos Indriago <carlos.indriago@tsoftglobal.com>
2021-10-17 13:22:18 -0300

    Added build command

--- build/server.js
+++ build/server.js
@@ -1,7 +1,21 @@
 "use strict";
-var express = require('express');
-var app = express();
-app.listen(3000, function () {
-    console.log('listening on 3000');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var database_service_1 = require("./services/database.service");
+var cards_router_1 = require("./routes/cards.router");
+var app = (0, express_1.default)();
+var port = 8080; // default port to listen
+(0, database_service_1.connectToDatabase)()
+    .then(function () {
+    app.use("/cards", cards_router_1.cardsRouter);
+    app.listen(port, function () {
+        console.log("Server started at http://localhost:" + port);
+    });
+})
+    .catch(function (error) {
+    console.error("Database connection failed", error);
+    process.exit();
 });
-console.log('May Node be with you');
